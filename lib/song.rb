c428f551da044121976b041322e60f2e7288ee86
Vaibhav Patel <vp932712@gmail.com>
2018-01-10 18:14:16 -0500

    unfinised

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,13 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # def initialize(name, artist_name)
+  # @name = name
+  # @artist_name = artist_name
+  # end
+
+
+
   def self.all
     @@all
   end
@@ -9,5 +16,21 @@ class Song
   def save
     self.class.all << self
   end
+
+def self.create
+  a = Song.new
+  @@all<< a
+  a
+end
+
+def self.create_by_name(string)
+  song = Song.new
+  song.name = string
+  @@all << (song)
+end
+
+
+
+
 
 end
