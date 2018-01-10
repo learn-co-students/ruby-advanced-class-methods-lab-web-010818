f75896da3a3e0056420671b428e1b56886f1f92b
GuttermanA <alexanderf.gutterman@gmail.com>
2018-01-10 14:32:09 -0500

    switching

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,29 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect { |song| song.name == name }
+  end
+
+  
+
+
 end
